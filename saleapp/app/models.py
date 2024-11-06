89980f97ebb04b36659221b8b65ab90aa518fea6
nguyentrunganhtuan201004@gmail.com <nguyentrunganhtuan201004@gmail.com>
2024-11-06 16:31:19 +0700

    update mysql & sqlalchemy

---
+++ saleapp/app/models.py
@@ -0,0 +1,79 @@
+from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
+from app import db, app
+from sqlalchemy.orm import relationship
+
+
+class Category(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False, unique=True)
+    products = relationship('product', backref='category', lazy=True)
+
+
+class Product(db.Model):
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(String(50), nullable=False)
+    description = Column(String(255), nullable=True)
+    price = Column(Float, default=0)
+    image = Column(String(100), nullable=True)
+    active = Column(Boolean, default=True)
+    category_id = Column(Integer, ForeignKey(Category.id), nullable=False)
+
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+        # c1 = Category(name='mobile')
+        # c2 = Category(name='Desktop')
+        # c3 = Category(name='Tablet')
+        # db.session.add_all([c1, c2, c3])
+        # db.session.commit()
+
+    products = [{
+        "id": 1,
+        "name": "iPhone 7 Plus",
+        "description": "Apple, 32GB, RAM: 3GB, iOS13",
+        "price": 17000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+        "category_id": 1
+    }, {
+        "id": 2,
+        "name": "iPad Pro 2020",
+        "description": "Apple, 128GB, RAM: 6GB",
+        "price": 37000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+        "category_id": 2
+    }, {
+        "id": 3,
+        "name": "Galaxy Note 10 Plus",
+        "description": "Samsung, 64GB, RAML: 6GB",
+        "price": 24000000,
+        "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+        "category_id": 1
+    },
+        {
+            "id": 4,
+            "name": "iPhone 7 Plus",
+            "description": "Apple, 32GB, RAM: 3GB, iOS13",
+            "price": 17000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647056401/ipmsmnxjydrhpo21xrd8.jpg",
+            "category_id": 1
+        }, {
+            "id": 5,
+            "name": "iPad Pro 2020",
+            "description": "Apple, 128GB, RAM: 6GB",
+            "price": 37000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1646729533/zuur9gzztcekmyfenkfr.jpg",
+            "category_id": 2
+        }, {
+            "id": 6,
+            "name": "Galaxy Note 10 Plus",
+            "description": "Samsung, 64GB, RAML: 6GB",
+            "price": 24000000,
+            "image": "https://res.cloudinary.com/dxxwcby8l/image/upload/v1647248722/r8sjly3st7estapvj19u.jpg",
+            "category_id": 1
+        }
+    ]
+    for p in products:
+        prod = Product(**p)
+        db.session.add(prod)
+        db.session.commit()
